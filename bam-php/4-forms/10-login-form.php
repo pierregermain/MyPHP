00836075e07392cea4044f824205e3fc83464680
Pierre Germain <pierre2008@gmail.com>
2017-06-07 13:30:31 -0500

    mejora login-form

--- bam-php/4-forms/10-login-form.php
+++ bam-php/4-forms/10-login-form.php
@@ -15,42 +15,59 @@ $usuarios = array(
 
 // Comprobar si se ha enviado el form
 if (isset($_REQUEST['action'])) {
-
   switch ($_REQUEST['action']) {
 
   case 'logout':
-    print 'logout';
+    session_destroy();
+    session_start();
+    $mensajes[] = "te has desconectado correctamente";
     break;
 
   case 'login':
-    print 'login';
+    // Buscamos si el usuario/contraseña introducido en el form existe en nuestro array $usuarios
+    foreach ($usuarios as $usuario => $pass) {
+      if ($_POST['username'] == $usuario && $_POST['password'] == $pass) {
+        // Asignamos variable de session
+        $_SESSION['username'] = $usuario;
+        break;
+      }
+    }
+    // Miramos si se asignó variable de Sessión
+    if (isset($_SESSION['username'])) {
+      $mensajes[] = 'Te has conectado correctamente';
+    }
+    else {
+      $mensajes[] = 'Comprueba tus creedenciales';
+    }
     break;
   }
-
 }
 
 
 // Generamos el form para loggearnos.
-// en el caso de no estarlo
+// en el caso de no estar loggeados
+// Ejemplo de uso de GET en la propia URL
 
 if (isset($_SESSION['username'])){
-  $mensajes[] = 'ya estás contectado :)';
+  $mensajes[] = 'listo para desconectarte ?';
   $output = '
-  <p> Bienvenido ...</p>';
+    <p> Bienvenido '. $_SESSION['username'] . '</p>
+    <b><p> Ejemplo de method GET en la propia URL:<p>
+    <p> <a href="10-login-form.php?action=logout"> Desconectarse</a></p></b>';
 } else {
-  $mensajes[] = 'no estás contectado :(';
+  $mensajes[] = 'listo para conectarte ?';
   $output = '
-  <form action="10-login-form.php" method="post">
-    <p>Usuario: <input type="text" name="username" /></p>
-    <p>Contraseña: <input type="text" name="password" /></p>
-    <p><input type="submit" value="Conectarse" /></p>
-    <input type="hidden" name="action" value="login" />
-  </form>';
+    <form action="10-login-form.php" method="post">
+      <p>Usuario: <input type="text" name="username" /></p>
+      <p>Contraseña: <input type="text" name="password" /></p>
+      <p><input type="submit" value="Conectarse" /></p>
+      <input type="hidden" name="action" value="login" />
+    </form>';
 }
 
 
 // Mostrar Mensajes
-// Ejemplo de uso de IMPLODE
+// Ejemplo de uso de IMPLODE: Mete </li><li> cuando hay más de un elemento en el aray
 $mensajes_output = '';
 if (count($mensajes) > 0) {
   $mensajes_output = '
