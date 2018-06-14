1995609802b750af60beb573c386e0c84cd3ad2d
Pierre Bertram <pierre.bertram@arcmedia.ch>
2018-06-14 16:23:37 +0200

    DB example adapted to php7

---
+++ bam-php/6-cms/03-site-with-db-and-login/settings/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$settings['company_name'] = 'AmaZING! Inc.';
+
+$settings['featured_product_ids'] = array(1, 2);
+
+// Database settings.
+$settings['db_username'] = 'admin';
+$settings['db_password'] = 'admin';
+$settings['db_server'] = 'localhost';
+$settings['db_database'] = 'amazing_inc'; 
